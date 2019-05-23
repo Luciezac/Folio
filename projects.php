5c626c184e0883203fdd0d91af8b2d30ea350a7e
Luciezac <lucie.zaccardi@hetic.net>
2019-05-23 09:29:07 +0200

    ça marche pas

--- projects.php
+++ projects.php
@@ -4,7 +4,7 @@
 <!-- COMMUNICATION -->
 <div class="main">
     <div class="column1">
-        <img class="illu_project_comm" src="assets/images/com_illu.svg" alt="">
+        <img class="illu_project_comm" src="assets/images/com_illu.svg" alt="illu comm">
     </div>
     
     <div class="column2">
@@ -17,12 +17,25 @@
 <!-- EVENT -->
 <div class="main">
     <div class="column1">
-        <img class="illu_project_event" src="assets/images/event_illu.svg" alt="">
+        <img class="illu_project_event" src="assets/images/event_illu.svg" alt="illu event">
     </div>
     
     <div class="column2">
         <p class="number_project">PROJECT N°2</p>
         <p class="category_project_event">EVENT</p>
+        <p class="descri_project">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione magni quasi vero eligendi! Fuga rem natus omnis molestias magnam a officia aliquid aut distinctio eum eos minus, obcaecati repellat rerum.</p>
+    </div>
+</div>
+
+<!-- DEV -->
+<div class="main">
+    <div class="column1">
+        <img class="illu_project_dev" src="assets/images/dev_illu.svg" alt="illu dev">
+    </div>
+    
+    <div class="column2">
+        <p class="number_project">PROJECT N°3</p>
+        <p class="category_project_dev">DEVELOPEMENT</p>
         <p class="descri_project">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione magni quasi vero eligendi! Fuga rem natus omnis molestias magnam a officia aliquid aut distinctio eum eos minus, obcaecati repellat rerum.</p>
     </div>
 </div>
--- views/partials/header.php
+++ views/partials/header.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="assets/styles/style.css">
+    <link rel="stylesheet" href="assets/styles/caca.css">
     <title>Home page</title>
 </head>
 
