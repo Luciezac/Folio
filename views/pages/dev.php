c6edcdc5ab8e866d1dd9cd18868962b9a10cd807
Luciezac <lucie.zaccardi@hetic.net>
2019-06-08 18:41:00 +0200

    lipstick: images

--- views/pages/dev.php
+++ views/pages/dev.php
@@ -1,2 +1,5 @@
 <?php include '../partials/header.php' ?>
 
+<div class=cat1>
+    <img src="../assets/images/article/developement.svg" alt="">
+</div>
--- views/partials/header.php
+++ views/partials/header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="assets/styles/style.css">
+    <link rel="stylesheet" href="http://localhost:8888/portfolio/assets/styles/style.css">
     <title>Home page</title>
 </head>
 
@@ -12,7 +12,7 @@
 
     <div class="menu">
         <a class="logo_link" href="index.php">
-            <img class="logo" src="assets/images/logo.svg" alt="logo">
+            <img class="logo" src="http://localhost:8888/portfolio/assets/images/logo.svg" alt="logo">
         </a>
 
         <div class="buttons">
